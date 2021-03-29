a855fe1de4074ab6463236f084ec683d7c9e0c01
MertvyeDushi <RomanTyurin@outlook.com>
2021-03-29 14:35:58 +0300

    Fix paths at server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path')
 const app = express()
 
 app.use(express.static(__dirname + '/build/'))
-app.use( '/', serveStatic( path.join(__dirname, '/build/templates') ) )
+app.use( '/', serveStatic( path.join(__dirname, '/build') ) )
 
 const port = 8080
 app.listen(port)
